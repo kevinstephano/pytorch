7a5e8a45e204e320e116545a59aeb9bce224c96e
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-09-02 09:50:08 -0700

    Added pointwise ops: addcdiv, addcmul
    ghstack-source-id: ae3c8994bb553b4495d7d0f33a4236677cad382c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42760

--- aten/src/ATen/native/ForeachOpsKernels.cpp
+++ aten/src/ATen/native/ForeachOpsKernels.cpp
@@ -10,7 +10,7 @@ std::vector<Tensor> foreach_tensor_##NAME##_scalar_kernel_slow(TensorList tensor
   std::vector<Tensor> result;                                                                             \
   result.reserve(tensors.size());                                                                         \
   for (const auto& t: tensors) {                                                                          \
-    result.emplace_back(t.NAME(scalar));                                                              \
+    result.emplace_back(t.NAME(scalar));                                                                  \
   }                                                                                                       \
                                                                                                           \
   return result;                                                                                          \
@@ -20,7 +20,7 @@ void foreach_tensor_##NAME##_scalar_kernel_slow_(TensorList tensors, Scalar scal
   check_foreach_api_restrictions(tensors);                                                                \
                                                                                                           \
   for (auto& t: tensors) {                                                                                \
-    t.NAME##_(scalar);                                                                                  \
+    t.NAME##_(scalar);                                                                                    \
   }                                                                                                       \
 }                                                                                                         \
                                                                                                           \
@@ -30,7 +30,7 @@ std::vector<Tensor> foreach_tensor_##NAME##_list_kernel_slow(TensorList tensors1
   std::vector<Tensor> result;                                                                             \
   result.reserve(tensors1.size());                                                                        \
   for (int i = 0; i < tensors1.size(); i++) {                                                             \
-    result.emplace_back(tensors1[i].NAME(tensors2[i]));                                               \
+    result.emplace_back(tensors1[i].NAME(tensors2[i]));                                                   \
   }                                                                                                       \
                                                                                                           \
   return result;                                                                                          \
@@ -39,8 +39,8 @@ std::vector<Tensor> foreach_tensor_##NAME##_list_kernel_slow(TensorList tensors1
 void foreach_tensor_##NAME##_list_kernel_slow_(TensorList tensors1, TensorList tensors2) {                \
   check_foreach_api_restrictions(tensors1, tensors2);                                                     \
                                                                                                           \
-  for (int i = 0; i < tensors1.size(); i++) {                                                              \
-    tensors1[i].NAME##_(tensors2[i]);                                                                   \
+  for (int i = 0; i < tensors1.size(); i++) {                                                             \
+    tensors1[i].NAME##_(tensors2[i]);                                                                     \
   }                                                                                                       \
 }
 
@@ -66,11 +66,38 @@ void foreach_tensor_##NAME##_slow_(TensorList tensors) {                   \
   }                                                                        \
 }
 
+#define FOREACH_POINTWISE_OP(NAME)                                                                                            \
+std::vector<Tensor> foreach_tensor_##NAME##_slow(TensorList input, TensorList tensors1, TensorList tensors2, Scalar scalar) { \
+  TORCH_CHECK(input.size() > 0, "Tensor list must have at least one tensor.");                                                \
+  TORCH_CHECK(input.size() == tensors1.size(), "Tensor lists must be of the same length.");                                   \
+  TORCH_CHECK(tensors1.size() == tensors2.size(), "Tensor lists must be of the same length.");                                \
+                                                                                                                              \
+  std::vector<Tensor> result;                                                                                                 \
+  for (int i = 0; i < input.size(); i++) {                                                                                    \
+    auto temp = input[i].NAME(tensors1[i], tensors2[i], scalar);                                                              \
+    result.emplace_back(temp);                                                                                                \
+  }                                                                                                                           \
+                                                                                                                              \
+  return result;                                                                                                              \
+}                                                                                                                             \
+                                                                                                                              \
+void foreach_tensor_##NAME##_slow_(TensorList input, TensorList tensors1, TensorList tensors2, Scalar scalar) {               \
+  TORCH_CHECK(input.size() > 0, "Tensor list must have at least one tensor.");                                                \
+  TORCH_CHECK(input.size() == tensors1.size(), "Tensor lists must be of the same length.");                                   \
+  TORCH_CHECK(tensors1.size() == tensors2.size(), "Tensor lists must be of the same length.");                                \
+                                                                                                                              \
+  for (int i = 0; i < input.size(); i++) {                                                                                    \
+    input[i].NAME##_(tensors1[i], tensors2[i], scalar);                                                                       \
+  }                                                                                                                           \
+}                                                                                                                             \
+
 FOREACH_BINARY_OP(add);
 FOREACH_BINARY_OP(sub);
 FOREACH_BINARY_OP(mul);
 FOREACH_BINARY_OP(div);
 FOREACH_UNARY_OP(sqrt);
 FOREACH_UNARY_OP(exp);
+FOREACH_POINTWISE_OP(addcdiv);
+FOREACH_POINTWISE_OP(addcmul);
 
 }} // namespace at::native
